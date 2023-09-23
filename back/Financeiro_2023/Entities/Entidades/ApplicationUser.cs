37ecc91cb42c0b4a4952ac3762ccb4acd9726a71
Carlos Alberto <carlosalbertode.campos@gmail.com>
2023-09-23 14:08:13 -0300

    Criando Entidades

---
+++ back/Financeiro_2023/Entities/Entidades/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Entities.Entidades
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Column("USER_CPF")]
+        public string CPF { get; set; } = string.Empty;
+    }
+}
---
+++ back/Financeiro_2023/Entities/Notificacoes/Notifica.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Notificacoes
+{
+    public class Notifica
+    {
+        [NotMapped]
+        public string NomePropriedade { get; set; } = string.Empty;
+
+        [NotMapped]
+        public string Mensagem { get; set; } = string.Empty;
+
+        [NotMapped]
+        public List<Notifica> Notificacoes { get; set; } = new List<Notifica>();
+
+        public bool ValidarPropriedadeString(string valor, string nomePropriedade)
+        {
+            if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifica
+                {
+                    Mensagem = "Campo obrigatório.",
+                    NomePropriedade = nomePropriedade,
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidarPropriedadeInt(int valor, string nomePropriedade)
+        {
+            if (valor <= 0 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifica
+                {
+                    Mensagem = "Campo obrigatório.",
+                    NomePropriedade = nomePropriedade,
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
